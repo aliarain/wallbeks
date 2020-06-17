f52f5d960ad711ec67fa396a7f71847bc40bbc61
Ali Arain <deistali2@gmail.com>
2020-06-17 12:02:35 +0500

    Database Initikize

--- lib/db/index.dart
+++ lib/db/index.dart
@@ -2,6 +2,7 @@
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:wallbeks/models/image_model.dart';
 
 class ImgDB{
   static const  dbName = 'wallbeks.db';
@@ -51,10 +52,79 @@ class ImgDB{
             uploadedTime TEXT NOT NULL,
             createdAt TEXT NOT NULL
           )''');  
-      }
+      },
+      onUpgrade: (db, oldVersion, newVersion) async {
+        print('[DEBUG] onUpgrade from $oldVersion to $newVersion');
+
+        if(oldVersion == 1){
+          await db.execute('''CREATE TABLE $tableDownloads(
+          id TEXT PRIMARY KEY UNIQUE NOT NULL,
+          name TEXT NOT NULL,
+          imageUrl TEXT NOT NULL,
+          createdAt TEXT NOT NULL,
+          )''');
+        }
+      },
     );
 
   }
+  Future<Null> close() async {
+    final client = await db;
+    await client.close();
+  }
+
+  Future<int> insertRecentImage(ImageModel image) async {
+    final values = (image..viewTime = DateTime.now()).toJson();
+    final dbClient = await db;
+    return await dbClient.insert(
+    tableRecent,
+    values,
+    conflictAlgorithm: ConflictAlgorithm.replace 
+    );
+  }
+   Future<int> deleteRecentImageById(String id) async {
+    final dbClient = await db;
+    return await dbClient.delete(
+      tableRecent,
+      where: 'id = ?',
+      whereArgs: [id]
+    );
+  }
+   Future<int> updateRecentImage(ImageModel image) async {
+    final dbClient = await db;
+    return await dbClient.update(
+    tableRecent,
+    image.toJson(),
+    where: 'id = ?',
+    whereArgs: [image.id],
+    conflictAlgorithm: ConflictAlgorithm.replace 
+    );
+  }
+   Future<int> deleteAllRecentImage(String id) async {
+    final dbClient = await db;
+    return await dbClient.delete(
+      tableRecent,
+      where: '1'
+    );
+  }
+  Future<ImageModel> getRecentImageById(String id) async {
+    final dbClient = await db;
+    final maps = await dbClient.query(
+      tableRecent,
+      where: 'id = ?',
+      whereArgs: [id],
+      limit: 1,
+    );
+    return maps.isNotEmpty
+        ? ImageModel.fromJson(id: maps.first['id'], json: maps.first)
+        : null;
+  }
+
+  
+
+  
+  
+
 }
   
  
